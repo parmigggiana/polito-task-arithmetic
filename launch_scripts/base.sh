98ba1055d8babeb0d69796356da771c6c18d909d
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2025-01-31 11:24:50 +0100

    add cache-dir to base script

--- launch_scripts/base.sh
+++ launch_scripts/base.sh
@@ -5,14 +5,17 @@
 python finetune.py \
 --data-location=./data/ \
 --save=./out/ \
+--cache-dir=./cache/ \
 --batch-size=32 \
 --lr=1e-4 \
 --wd=0.0
 
 python eval_single_task.py \
 --data-location=./data/ \
---save=./out/
+--save=./out/ \
+--cache-dir=./cache/ \
 
 python eval_task_addition.py \
 --data-location=./data/ \
---save=./out/
+--save=./out/ \
+--cache-dir=./cache/ \
