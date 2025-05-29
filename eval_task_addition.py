c8cccbbd52424e67b9f3e880abec6b75ea423691
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2025-05-29 13:45:55 +0200

    fix file name

--- eval_task_addition.py
+++ eval_task_addition.py
@@ -78,7 +78,7 @@ def eval_task_addition(args):
 
     for dataset_name in datasets:
         print(f"Creating task vector for {dataset_name}...")
-        pretrained_model_path=os.path.join(args.save, f'base_{dataset_name}.pt')
+        pretrained_model_path=os.path.join(args.save, f'base.pt')
         finetuned_model_path = f"./out/finetuned_{dataset_name}.pt"
         task_vector = NonLinearTaskVector(pretrained_checkpoint=pretrained_model_path,
                                           finetuned_checkpoint=finetuned_model_path)
