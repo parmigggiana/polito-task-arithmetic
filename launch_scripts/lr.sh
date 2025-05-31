01e81c914097e2ea1d661a3536dfa5dc2533625d
Roberto Montesanto <39373241+Roberto34@users.noreply.github.com>
2025-05-31 05:47:05 +0200

    Update lr.sh

--- launch_scripts/lr.sh
+++ launch_scripts/lr.sh
@@ -1,32 +1,8 @@
-python finetune.py \
---data-location=./data/ \
---save=./out_lr_3/ \
---cache-dir=./cache/ \
---batch-size=32 \
---lr=5e-4
-
-python eval_single_task.py \
---data-location=./data/ \
---save=./out_lr_3/ \
---cache-dir=./cache/ \
-
 python eval_task_addition.py \
 --data-location=./data/ \
 --save=./out_lr_3/ \
 --cache-dir=./cache/ \
 --alpha=0.15
-
-python finetune.py \
---data-location=./data/ \
---save=./out_lr_2/ \
---cache-dir=./cache/ \
---batch-size=32 \
---lr=5e-5
-
-python eval_single_task.py \
---data-location=./data/ \
---save=./out_lr_2/ \
---cache-dir=./cache/
 
 python eval_task_addition.py \
 --data-location=./data/ \
